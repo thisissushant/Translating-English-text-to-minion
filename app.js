2727ef806e2ae2c11a50f621d31a040a868b9093
Sushant Singh <sushantbibhu@gmail.com>
2023-06-19 18:21:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("txt-input");
+var txtInput = document.querySelector("#txt-input");
 
 console.log(txtInput)
 
 function clickHandler(){
     console.log("clicked!");
     console.log("input", txtInput.value);
-
 };
-
-btnTranslate.addEventListener("click", function clickHandler())
+btnTranslate.addEventListener("click", function clickHandler)
